236fd8f7025a4f3c146918eae7d8a9c5092d533d
prueba2@gmail.com <prueba@gmail.com>
2024-12-09 17:12:52 -0500

    primer commit

---
+++ src/app/components/counter.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html'
+})
+export class CounterComponent {
+
+
+  title: string = 'Counter usando redux!'
+  counter: number;
+
+  constructor(){
+    this.counter = 0;
+  }
+
+  increment(): void{
+    this.counter++;
+    console.log('incrementando ...');
+  }
+
+  decrement(): void{
+    this.counter--;
+    console.log('decrementando ...')
+  }
+
+  reset(): void{
+    this.counter=0;
+    console.log('reset el contador ...')
+  }
+
+}
