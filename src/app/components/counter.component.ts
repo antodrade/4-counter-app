ed57000ccbbb282f3505d161e59a255b8cf31989
prueba2@gmail.com <prueba@gmail.com>
2024-12-10 17:28:00 -0500

    Despachando acciones en el componente con Dispatch

--- src/app/components/counter.component.ts
+++ src/app/components/counter.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { decrement, increment, reset } from '../store/items.action';
 
 @Component({
   selector: 'app-counter',
@@ -12,22 +14,28 @@ export class CounterComponent {
   title: string = 'Counter usando redux!'
   counter: number;
 
-  constructor(){
+  constructor(private store: Store<{counter: number}>){
     this.counter = 0;
+    this.store.select('counter').subscribe( count => {
+      this.counter = count;
+    })
   }
 
   increment(): void{
-    this.counter++;
+    // this.counter++;
+    this.store.dispatch(increment());
     console.log('incrementando ...');
   }
 
   decrement(): void{
-    this.counter--;
+    // this.counter--;
+    this.store.dispatch(decrement());
     console.log('decrementando ...')
   }
 
   reset(): void{
-    this.counter=0;
+    // this.counter=0;
+    this.store.dispatch(reset());
     console.log('reset el contador ...')
   }
 
